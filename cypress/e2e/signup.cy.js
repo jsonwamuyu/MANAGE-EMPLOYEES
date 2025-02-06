ba85e06d85bb9b03ffc741e5b8d41a474f84c8a2
jsonwamuyu <jysonmuchiri@gmail.com>
2025-02-06 16:23:41 +0300

    Add signup stub

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,32 +1,27 @@
-/// <reference types="Cypress" />
+/// <reference types='Cypress'/>
 
-describe("Test sign up page", () => {
+describe("Test signup page", () => {
   beforeEach(() => {
     cy.visit("signup.html");
   });
 
-  it("Should sign up successfully ", () => {
-    // stub the API request to prevent calling the real backend
-    cy.intercept("POST", "/api/signup", {
+  it("Should sign up user provided correct credentials", () => {
+    cy.intercept("POST", "/app/api", {
       statusCode: 201,
-      body: { message: "Signup successful!" },
-    }).as("signupRequest");
-
-    // visit the signup page
-    cy.visit("/signup.html");
-
-    // Fill out the signup form and click signup button
-    cy.get('[data-cy="signup-email-input"]').type("admin@example.com");
-    cy.get('[data-cy="signup-password-input"]').type("admin123");
-    cy.get('[data-cy="signup-button"]').click();
-
-    // Wait for the stub request
-    cy.wait("@signupRequest").then((interception) => {
-      expect(interception.response.statusCode).to.eq(201);
-      expect(interception.response.body.message).to.eq("Signup successful!");
-    });
-
-    // Check if a successful message was displayed
-    cy.contains("Signup successful").should("be.visible");
+      body: { message: "Signup successful" },
+    }).as("signupStub");
   });
+
+  // Type input into the signup form
+  cy.get('[data-cy="email"]').type("admin@example.com");
+  cy.get('[data-cy="password"]').type = "admin123";
+  cy.get('[data-cy="submit-button"]').click();
+
+  //
+  cy.wait("@signupStub").then((intercept) => {
+    expect(intercept.response.statusCode).to.eq(201);
+    expect(intercept.response.body.message).to.eq("Signup successful");
+  });
+
+  cy.contains("Signup successful").should("be.visible");
 });
